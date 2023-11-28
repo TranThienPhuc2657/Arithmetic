6607a51543e6ff8f175d67568ddea8c5677f36dc
TranThienPhuc2657 <145333256+TranThienPhuc2657@users.noreply.github.com>
2023-11-28 10:29:33 +0700

    SPP problem in BTSH.pdf
    A problem about finding all abundant numbers.
    Abundant number is a number which has the restricted divisor sum (the sum of its divisors (except itself)) larger than itself.

---
+++ SPP.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define file(name) if (fopen(name".inp","r")) {freopen(name".inp","r",stdin); freopen(name".out","w",stdout);}
+#define pb push_back
+#define fi first
+#define se second
+#define REP(i,n) for (int i=0; i<n; i++)
+#define REPD(i,n) for (int i=n-1; i>=0; i--)
+#define FOR(i,a,b) for (int i=a; i<=b; i++)
+#define FORD(i,a,b) for (int i=a; i>=b; i--)
+#define ll long long
+#define vi vector <int>
+#define vll vector <ll>
+const ll INF=LLONG_MAX;
+const int N=1e6+5;
+
+int l,r,cnt=0;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    file("SPP");
+
+    cin >> l >> r;
+    vi a(r-l+1,1);
+    if (l==1) a[0]=0;
+    FOR(i,2,r/2)
+        for(int j=max(2*i,(l+i-1)/i*i); j<=r; j+=i)
+            a[j-l]+=i;
+    REP(i,a.size()) if (a[i]>i+l) cnt++;
+    cout << cnt;
+    return 0;
+}
